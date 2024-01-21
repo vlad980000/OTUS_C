216e2af7506453ba053ff65440390c3903a50c63
vladislav chernishev <vlad980000@mail.ru>
2024-01-21 21:34:42 +0300

    please_help

--- Rarjpeg/first_work.c
+++ Rarjpeg/first_work.c
@@ -3,15 +3,38 @@
 #include <string.h>
 
 #define ZIP_MAGIC_NUMBER 0x504B0304
+#pragma pack(1)
+struct CentralDirectory{
+    unsigned int signature;
+    unsigned short version;
+    unsigned short versionNeeded;
+    unsigned short flags;
+    unsigned short compression;
+    unsigned short modtime;
+    unsigned short MmodDate;
+    unsigned int crc32;
+    unsigned int compressedSize;
+    unsigned int uncompressedSize;
+    unsigned short fileNameLength;
+    unsigned short extraFieldLength;
+    unsigned short fileCommLength;
+    unsigned short diskStart;
+    unsigned short internalFileAttributes;
+    unsigned int externalFileAttributes;
+    unsigned int offsetOfLocalHeader;
+};
 
 int main(void)
 {
     char filename[512];
 
     FILE *file;
+    int bytes = 0;
     unsigned char jpeg_header[] = {0xFF, 0xD8};
-    unsigned char zip_magic_header[] = {0x50, 0x4B};
-    unsigned char buffer[2];
+    unsigned char zip_magic_header[] = {0x50, 0x4B ,0x03, 0x04};
+    unsigned char zip_magic_header_empty[] = {0x50, 0x4B ,0x05, 0x06};
+    unsigned char zip_central_header[] = {0x50, 0x4B ,0x01, 0x02};
+    unsigned char buffer[4];
 
     printf("Введите путь к файлу: ");
     fgets(filename, sizeof(filename), stdin);
@@ -33,14 +56,38 @@ int main(void)
     for (int i = fileSize - 2 ; i >= 0 ; i -= 2){
         fseek(file, i, SEEK_SET);
         if (fread(buffer, 1, sizeof(buffer), file) == sizeof(buffer)){
-            if (buffer[0] == zip_magic_header[0] && buffer[1] == zip_magic_header[1]){
-                printf("В файле зашифрован ZIP архив\n");
-                break;
+            /*for (int j = 0; j < sizeof(buffer); ++j) {
+                printf("%02X", buffer[j]);
+            }*/
+            //printf("\n");
+            if (buffer[0] == zip_central_header[0] && buffer[1] == zip_central_header[1] && buffer[2] == zip_central_header[2] && buffer[3] == zip_central_header[3]) {
+                printf("В файле зашифрован ZIP архив файлы внутри\n"); 
+                struct CentralDirectory cdRecord;
+                fread(&cdRecord,sizeof(struct CentralDirectory), 1, file );
+                fseek(file, i + sizeof(struct CentralDirectory), SEEK_SET);
+                char *fileName = (char *)malloc(cdRecord.fileNameLength + 1);
+                fread(fileName, 1, cdRecord.fileNameLength, file);
+                fileName[cdRecord.fileNameLength] = '\0';
+                printf("Signature: %08X\n", cdRecord.signature);
+                printf("Version: %hu\n", cdRecord.version);
+                printf("Version Needed: %hu\n", cdRecord.versionNeeded);
+                printf("Flags: %hu\n", cdRecord.flags);
+                printf("Compression: %hu\n", cdRecord.compression);
+                printf("Modtime: %hu\n", cdRecord.modtime);
+                printf("MmodDate: %hu\n", cdRecord.MmodDate);
+                printf("CRC32: %08X\n", cdRecord.crc32);
+                printf("Compressed Size: %u\n", cdRecord.compressedSize);
+                printf("Uncompressed Size: %u\n", cdRecord.uncompressedSize);
+                printf("File Name Length: %hu\n", cdRecord.fileNameLength);
+                printf("Extra Field Length: %hu\n", cdRecord.extraFieldLength);
+                printf("File Comment Length: %hu\n", cdRecord.fileCommLength);
+                printf("Disk Start: %hu\n", cdRecord.diskStart);
+                printf("Internal File Attributes: %hu\n", cdRecord.internalFileAttributes);
+                printf("External File Attributes: %08X\n", cdRecord.externalFileAttributes);
+                printf("Offset of Local Header: %08X\n", cdRecord.offsetOfLocalHeader);
+                free(fileName);
             }
-            else if(buffer[0] == jpeg_header[0] && buffer[1] == jpeg_header[1]){
-                printf("В файле не зашифрован ZIP архив\n");
-                break;
-            }
+            //printf("\n");
         }
     }
     fclose(file);
